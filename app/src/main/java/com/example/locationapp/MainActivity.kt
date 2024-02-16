0bf17c16b90aa5b091d96b9c06b5bfcb3adc1b9c
Nagma Bano <nagma.bano@sap.com>
2024-02-17 00:38:14 +0530

    Requesting for permission and seeing the popup

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.locationapp
 import android.content.Context
 import android.os.Bundle
 import android.Manifest
+import android.health.connect.datatypes.ExerciseRoute.Location
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -17,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.locationapp.ui.theme.LocationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MyApp()
                 }
             }
         }
@@ -38,7 +42,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DisplayLocation(locationUtils: LocationUtils,
+fun MyApp() {
+    val context = LocalContext.current
+    val LocationUtils = LocationUtils(context)
+    LocationDisplay(locationUtils = LocationUtils, context = context)
+}
+
+@Composable
+fun LocationDisplay(locationUtils: LocationUtils,
                     context: Context) {
 
     val requestPermissionLauncher = rememberLauncherForActivityResult(
@@ -50,6 +61,22 @@ fun DisplayLocation(locationUtils: LocationUtils,
                 // I have access to location
             } else {
                 // Ask for permission
+                val rationalRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+
+                if (rationalRequired) {
+                    Toast.makeText(context, "Location permission is required for this feature to work",
+                        Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context, "Location permission is required for this feature." +
+                            "Please enable it in android settings",
+                        Toast.LENGTH_LONG).show()
+                }
             }
         })
 
@@ -64,6 +91,12 @@ fun DisplayLocation(locationUtils: LocationUtils,
                 // Permission already granted, update the location
             } else {
                 // Request location permission
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
             }
         }) {
             Text(text = "Get Location")
